c4e3e959828de4401981f9104197f8c0bf6aca24
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2014-11-30 18:25:31 +0000

    Update travis.js

--- travis.js
+++ travis.js
@@ -1,6 +1,6 @@
-#!/bin/env node
+#!/usr/bin/env node
 var cp = require('child_process');
 var bickle = 'bickle builds stpettersens/genericFunctions-for-node -n 5';
 cp.exec(bickle, function(err, stdout, stderr) {
-  console.log(stdout);
+	console.log(stdout);
 });
